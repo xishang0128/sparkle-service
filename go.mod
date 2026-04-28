04c689107ba9540274061fa8f895f1ba814ee64e
xishang0128 <xishang02@gmail.com>
2026-04-28 19:35:39 +0800

    support structured service logs

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/shirou/gopsutil/v4 v4.26.3
 	github.com/spf13/cobra v1.10.2
 	github.com/xishang0128/sysproxy-go v0.0.0-20260426195823-de059c4f0a41
+	go.uber.org/zap v1.27.1
 	golang.org/x/sys v0.43.0
 )
 
@@ -23,4 +24,5 @@ require (
 	github.com/tklauser/go-sysconf v0.3.16 // indirect
 	github.com/tklauser/numcpus v0.11.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 )
--- log/log.go
+++ log/log.go
@@ -1,10 +1,73 @@
 package log
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	stdlog "log"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/buffer"
+	"go.uber.org/zap/zapcore"
 )
+
+var (
+	logger         = newLogger(zapcore.AddSync(os.Stderr))
+	loggerMu       sync.RWMutex
+	sugar          = logger.Sugar()
+	jsonBufferPool = buffer.NewPool()
+)
+
+type jsonPrettyEncoder struct {
+	zapcore.Encoder
+	pretty     bool
+	stackArray bool
+}
+
+func (e *jsonPrettyEncoder) Clone() zapcore.Encoder {
+	return &jsonPrettyEncoder{
+		Encoder:    e.Encoder.Clone(),
+		pretty:     e.pretty,
+		stackArray: e.stackArray,
+	}
+}
+
+func (e *jsonPrettyEncoder) EncodeEntry(ent zapcore.Entry, fields []zapcore.Field) (*buffer.Buffer, error) {
+	buf, err := e.Encoder.EncodeEntry(ent, fields)
+	if err != nil || (!e.pretty && !e.stackArray) {
+		return buf, err
+	}
+
+	var obj map[string]any
+	if err := json.Unmarshal(buf.Bytes(), &obj); err != nil {
+		return buf, nil
+	}
+
+	if e.stackArray {
+		if stack, ok := obj["stacktrace"].(string); ok && stack != "" {
+			obj["stacktrace"] = strings.Split(stack, "\n")
+		}
+	}
+
+	var out []byte
+	if e.pretty {
+		out, err = json.MarshalIndent(obj, "", "  ")
+	} else {
+		out, err = json.Marshal(obj)
+	}
+	if err != nil {
+		return buf, nil
+	}
+	out = append(out, '\n')
+
+	newBuf := jsonBufferPool.Get()
+	newBuf.AppendBytes(out)
+	buf.Free()
+	return newBuf, nil
+}
 
 func currentLogPath() string {
 	return filepath.Join(os.TempDir(), "sparkle-service.log")
@@ -34,29 +97,77 @@ func InitLogging() (*os.File, error) {
 		return nil, err
 	}
 
-	logFile, err := os.OpenFile(currentLogPath(), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	logFile, err := os.OpenFile(currentLogPath(), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 	if err != nil {
 		return nil, err
 	}
 	os.Stdout = logFile
 	os.Stderr = logFile
 
-	log.SetOutput(logFile)
+	setLogger(newLogger(zapcore.AddSync(logFile)))
+	redirectStdLog()
 	return logFile, nil
 }
 
+func newLogger(ws zapcore.WriteSyncer) *zap.Logger {
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoder := &jsonPrettyEncoder{
+		Encoder:    zapcore.NewJSONEncoder(encoderCfg),
+		pretty:     true,
+		stackArray: true,
+	}
+	return zap.New(zapcore.NewCore(encoder, ws, zapcore.InfoLevel), zap.AddCaller(), zap.AddCallerSkip(1))
+}
+
+func setLogger(newLog *zap.Logger) {
+	loggerMu.Lock()
+	defer loggerMu.Unlock()
+	logger = newLog
+	sugar = newLog.Sugar()
+}
+
+func L() *zap.Logger {
+	loggerMu.RLock()
+	defer loggerMu.RUnlock()
+	return logger
+}
+
+func S() *zap.SugaredLogger {
+	loggerMu.RLock()
+	defer loggerMu.RUnlock()
+	return sugar
+}
+
+func Sync() {
+	loggerMu.RLock()
+	defer loggerMu.RUnlock()
+	if logger != nil {
+		_ = logger.Sync()
+	}
+}
+
+func redirectStdLog() {
+	stdLogger, err := zap.NewStdLogAt(L().WithOptions(zap.AddCallerSkip(1)), zap.InfoLevel)
+	if err != nil {
+		return
+	}
+	stdlog.SetOutput(stdLogger.Writer())
+	stdlog.SetFlags(0)
+}
+
 func Fatal(v ...any) {
-	log.Fatal(v...)
+	S().Fatal(fmt.Sprint(v...))
 }
 
 func Print(v ...any) {
-	log.Print(v...)
+	S().Info(fmt.Sprint(v...))
 }
 
 func Printf(format string, v ...any) {
-	log.Printf(format, v...)
+	S().Infof(format, v...)
 }
 
 func Println(v ...any) {
-	log.Println(v...)
+	S().Info(strings.TrimSuffix(fmt.Sprintln(v...), "\n"))
 }
